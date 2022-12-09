bccf243be7411459cd67ca2db4c17416469395ef
Hakim <goldentechnology10@outlook.fr>
2022-12-09 16:48:48 +0100

    update jenkisfile api

--- segma_api.groovy
+++ segma_api.groovy
@@ -3,7 +3,7 @@
 pipeline {
     agent any
     tools {
-        maven 'Maven 3.3.9'
+        maven 'Maven 3.5.4'
         jdk 'jdk11'
     }
     stages {
@@ -15,25 +15,14 @@ pipeline {
               '''
             }
       }
-      stage('D') {
+      stage('Build') {
         steps {
           sh 'mvn package'
         }
       post {
         success {
           sh 'cp target/*.jar /home/adm1/articatory/'
-          sh 'ansible-playbook playbook.yml'
-        }
-      }
-    }
-      stage('Build de l\'api') {
-        steps {
-          sh 'mvn package'
-        }
-      post {
-        success {
-          sh 'cp target/*.jar /home/adm1/articatory/'
-          sh 'ansible-playbook playbook.yml'
+          sh 'ansible-playbook playbook_sigma.yml'
         }
       }
     }
