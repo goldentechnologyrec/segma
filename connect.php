e9f8668a54f6008482466ff6583ec0f795a3275e
goldentechnologyrec <119066198+goldentechnologyrec@users.noreply.github.com>
2022-12-09 15:25:05 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("host=192.168.193.139 port=5432  user=caux password=caux dbname=caux");
+$conn = pg_connect("host=192.168.193.139 port=5432  user=emega password=omega dbname=omega");
 if(!$conn){
     echo "Database connection failed. Error:".$conn->error;
     exit;
